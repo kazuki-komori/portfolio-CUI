14b86d8df981268d6381b9548794935c7cd7866e
kazuki-komori <komokazu.nankai@gmail.com>
2021-01-11 18:43:09 +0900

    fix: deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  purge: ['./components/**/*.tsx', './pages/**/*.tsx'],
+  // purge: ['./components/**/*.tsx', './pages/**/*.tsx'],
+  purge: ['./**/*.tsx'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
